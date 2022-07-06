b91f35f71bdf7268830b94edb88aef1ce5cc9371
still_Jerry <oljaapelsin@gmail.com>
2022-07-06 13:04:12 +0300

    Initial Commit

---
+++ TryForServer/TryForServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryForServerApp.swift
+//  TryForServer
+//
+//  Created by Anatolich Mixaill on 06.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct TryForServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
